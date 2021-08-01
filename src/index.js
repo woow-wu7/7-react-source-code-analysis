9899cf5d929fdc4a1241cc3dc73b5a3a82f85794
woow_wu7 <wuxia@shouqianba.com>
2021-08-01 22:49:09 +0800

    feat: comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-import * as React from 'react';
+import * as React from 'react'; // 这里能够引用到，是因为在webpack.config.js中的resolve.alias中指定了别名的引用路径
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
